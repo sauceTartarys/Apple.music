86a1a8be8448021bed01f89ea683c1bf17d6f3c5
sauceTartarys <136168621+sauceTartarys@users.noreply.github.com>
2024-12-29 18:26:04 +0200

    refrttr

--- runserver.py
+++ runserver.py
@@ -5,8 +5,8 @@ from DBMusic import DBMusic
 app = Flask("Music")
 db_name = "music.db"
 database = [
-    (0,"We are the danger", "Ця після для любителів", "Jay Maroni, Voelker Bros",1517737,'пісня','https://www.youtube.com/watch?v=Ze9UBqiha9g'),
-    (0,"Daylight", "Ця пісня була популярна в 2023р ", "гурт пісні: David Kushner, Rob Kirvan, Rick Hornby, Hayden Robert Hubers", 367276920, "пісня","https://www.youtube.com/watch?v=MoN9ql6Yymw")
+    (0,"We are the danger", "Ця після для любителів", "Jay Maroni, Voelker Bros",1517737,'пісня','https://www.youtube.com/watch?v=Ze9UBqiha9g',"maxresdefault.jpg"),
+    (0,"Daylight", "Ця пісня була популярна в 2023р ", "гурт пісні: David Kushner, Rob Kirvan, Rick Hornby, Hayden Robert Hubers", 367276920, "пісня","https://www.youtube.com/watch?v=MoN9ql6Yymw","maxresdefault.jpg")
 ]
 
 
@@ -15,4 +15,4 @@ def index():
     db_manager = DBMusic(db_name)
     return render_template("index.html",musicals=database)
 
-app.run(port=8001)
+app.run(port=8002)
