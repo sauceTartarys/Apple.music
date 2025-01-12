1c6448d0ebe5c62faa79bee217346b116808a371
sauceTartarys <136168621+sauceTartarys@users.noreply.github.com>
2025-01-12 15:43:24 +0200

    reshdn

--- runserver.py
+++ runserver.py
@@ -5,8 +5,8 @@ from DBMusic import DBMusic
 app = Flask("Music")
 db_name = "music.db"
 database = [
-    (0,"We are the danger", "Ця після для любителів", "Jay Maroni, Voelker Bros",1517737,'пісня','https://www.youtube.com/watch?v=Ze9UBqiha9g',"maxresdefault.jpg"),
-    (0,"Daylight", "Ця пісня була популярна в 2023р ", "гурт пісні: David Kushner, Rob Kirvan, Rick Hornby, Hayden Robert Hubers", 367276920, "пісня","https://www.youtube.com/watch?v=MoN9ql6Yymw","maxresdefault.jpg")
+    (0,"We are the danger", "Цей рок для любителів", "Jay Maroni, Voelker Bros",1517737,'пісня','https://www.youtube.com/watch?v=Ze9UBqiha9g',"maxresdefault.jpg"),
+    (0,"Daylight", "Цей рок був популярний в 2023р ", "гурт пісні: David Kushner, Rob Kirvan, Rick Hornby, Hayden Robert Hubers", 367276920, "пісня","https://www.youtube.com/watch?v=MoN9ql6Yymw","maxresdefault.jpg")
 ]
 
 
